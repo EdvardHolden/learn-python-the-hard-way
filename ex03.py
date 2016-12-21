287af0c596e459ebef728c4d7ca2882fb9e9f87f
Edvard Holden <edvard.holden@student.manchester.ac.uk>
2016-12-21 18:57:47 +0100

    Finish ex03
    finished ex03 which is about basic math operators

---
+++ ex03.py
@@ -0,0 +1,17 @@
+#Program to get used to the common math operators
+print "Counting chickens:"
+print "Hens", 25 + 30 / 6 #EO 30
+print "Roosters", 100 - 25 * 3 % 4 # EO 97
+
+print "Counting eggs:",
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 #EO 7
+
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 2 - 7 #EO False
+
+print "What is 3 + 2? ", 3 + 2 # EO 5
+print "What is 5 - 7? ", 5 - 7 # EO -2
+
+print "Is it greater?", 5 > -2 # EO True
+print "Is it greater or equal?", 5 >= -2 # EO True
+print "IS it less than or equal? ", 5 <= -2 # EO False
