c188b619a1698668bffd35e9f8bf36d67d465ecd
Edvard Holden <edvard.holden@student.manchester.ac.uk>
2016-12-26 18:58:39 +0100

    Finish ex38, and ex39
    Finished  ex38, and ex39 which uses more lists and introduces dictionaries

---
+++ ex38.py
@@ -0,0 +1,24 @@
+# Some basic operations on lists
+
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print "Adding: ", next_one
+    stuff.append(next_one)
+    print "There are %d items now." % len(stuff)
+
+print "The current list: ", stuff
+
+print "Operations on the list"
+
+print stuff[1]
+print stuff[-1]
+print stuff.pop()
+print ' '.join(stuff)
+print '#'.join(stuff[3:5])
+
+
